372afa1af4e86e6c185a6b7a872746330c67dbb2
mad-cow <t.coehoorn@mad-cow.ch>
2018-03-06 00:54:12 +0100

    Adding Course Buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,11 @@ $(function () {
         dataType: 'jsonp',
         success: function (response) {
             for ( let i = 0; i < response.courses.completed.length; i++ ) {
-                $('#badges').append('<div class="course"><h3>' + response.courses.completed[i].title + '</h3><img src="' + response.courses.completed[i].badge + '"></div>');
+                $('#badges').append('<div class="course">' +
+                    '<h3>' + response.courses.completed[i].title + '</h3>' +
+                    '<img src="' + response.courses.completed[i].badge + '">' +
+                    '<a href="'+ response.courses.completed[i].url +'" target="_blank" class="btn btn-primary">See Course</a>' +
+                    '</div>');
             }
         }
     });
