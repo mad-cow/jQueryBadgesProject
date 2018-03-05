8a2f42eff6893f5b5b2da822cf5347a615ca5ac3
mad-cow <t.coehoorn@mad-cow.ch>
2018-03-06 00:41:49 +0100

    Make an ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+    // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/mad_cow.json',
+        dataType: 'jsonp',
+        success: function (response) {
 
+        }
+    });
 });
